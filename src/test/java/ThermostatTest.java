d5b3af3a6b9e9483106148fd353289ffc86c4cfe
cscneko <ht94045.cs06@nctu.edu.tw>
2022-05-22 13:19:29 +0800

    finish test5~9

--- src/test/java/ThermostatTest.java
+++ src/test/java/ThermostatTest.java
@@ -69,4 +69,48 @@ class ThermostatTest {
         assertEquals(false, testHeat.getHeaterOn());
         // overTemp - Current temp
     }
+    @org.junit.jupiter.api.Test
+    void test_5(){
+        ProgrammedSettings FakeSt = mock(ProgrammedSettings.class);
+        when(FakeSt.getSetting(any(Period.class), any(DayType.class))).thenReturn(28); //a-dtemp
+        testHeat.setOverTemp(26);
+        assertEquals(true, testHeat.turnHeaterOn(FakeSt)); //p
+        assertEquals(true, testHeat.getHeaterOn());
+        assertEquals(26-26, testHeat.getRunTime());
+    }
+    @Test
+    void test_6(){
+        ProgrammedSettings FakeSt = mock(ProgrammedSettings.class);
+        when(FakeSt.getSetting(any(Period.class), any(DayType.class))).thenReturn(26);
+        testHeat.setOverTemp(26);
+        assertEquals(false, testHeat.turnHeaterOn(FakeSt)); //p
+        assertEquals(false, testHeat.getHeaterOn());
+        assertEquals(26-26, testHeat.getRunTime());
+    }
+    @Test
+    void test_7(){
+        ProgrammedSettings FakeSt = mock(ProgrammedSettings.class);
+        when(FakeSt.getSetting(any(Period.class), any(DayType.class))).thenReturn(26);
+        testHeat.setOverride(false);
+        assertEquals(false, testHeat.turnHeaterOn(FakeSt)); //p
+        assertEquals(false, testHeat.getHeaterOn());
+        assertEquals(26-26, testHeat.getRunTime());
+    }
+    @Test
+    void test_8(){
+        ProgrammedSettings FakeSt = mock(ProgrammedSettings.class);
+        when(FakeSt.getSetting(any(Period.class), any(DayType.class))).thenReturn(28);
+        assertEquals(true, testHeat.turnHeaterOn(FakeSt)); //p
+        assertEquals(true, testHeat.getHeaterOn());
+        assertEquals(28-26, testHeat.getRunTime());
+    }
+    @Test
+    void test_9(){
+        ProgrammedSettings FakeSt = mock(ProgrammedSettings.class);
+        when(FakeSt.getSetting(any(Period.class), any(DayType.class))).thenReturn(28);
+        testHeat.setTimeSinceLastRun(3);
+        assertEquals(false, testHeat.turnHeaterOn(FakeSt)); //p
+        assertEquals(false, testHeat.getHeaterOn());
+        assertEquals(26-26, testHeat.getRunTime());
+    }
 }
